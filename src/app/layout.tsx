b340a297e3129d0e0d7f8afc842c33fee24e7d73
Ahmedfahmy8308 <fhmy8308@gmail.com>
2025-05-17 23:07:16 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -69,7 +69,7 @@ export default function RootLayout({
     <html lang="en" className="dark" suppressHydrationWarning>
       <head>
         <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css" />
-        <link rel="canonical" href="https://ahmed-fahmy.com" />
+        <link rel="canonical" href="https://ahmedfahmy.me" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="format-detection" content="telephone=no" />
         <meta name="theme-color" content="#4338ca" />
